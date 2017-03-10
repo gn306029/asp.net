d2d3dc3e3326657347801e15679593dc7965fa7a
gn306029 <1104137110@gm.kuas.edu.tw>
2017-03-10 16:50:03 +0800

    影片3-1 3-2進度

---
+++ WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        /// <summary>
+        /// 訂單管理系統首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("123");
+            ViewBag.CustomerID = order.CustomerID;
+            ViewBag.CustomerName = order.CustomerName;
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的Action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Class1 order)
+        {
+            return View("Index2");
+        }
+
+    }
+}
---
+++ WebApplication1/Models/OrderService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+        /// <summary>
+        /// 刪除訂單 
+        /// </summary>
+        public void DeleteOrderByID()
+        {
+
+        }
+        /// <summary>
+        /// 修改訂單
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Models.Class1 GetOrderById(string id)
+        {
+            Models.Class1 result = new Class1();
+            result.CustomerID = 123;
+            result.CustomerName = "瑞陽資訊";
+            return result;
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Class1> GetOrders()
+        {
+            return new List<Class1>();
+        }
+    }
+}
